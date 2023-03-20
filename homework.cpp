370c81a3727c0c8c770a7989ec41e54d01a9654e
Matviy123 <55127584+Matviy123@users.noreply.github.com>
2023-03-20 17:49:16 +0200

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 int main()
 {
-    //Завдання 1
+    //Task 1
     srand(time(0));
     setlocale(LC_CTYPE, "ukr");
     map <int, int> resultRolls;
-    int n; // Скільки раз кинути кубік
+    int n; 
     int roll;
-    cout << "Введiть кiлькiсть кидкiв кубика : ";
+    cout << "Number of attempts : ";
     cin >> n;
     for (int i = 0; i < n; i++)
     {
@@ -33,55 +33,55 @@ int main()
             numberLeast = x.first;
         }
     }
-    cout << "Найбiльше випало число пiд номером : " << numberMost << endl;
-    cout << "Найменше випало число пiд номером : " << numberLeast << endl;
+    cout << "Most fell  : " << numberMost << endl;
+    cout << "Most least : " << numberLeast << endl;
 
-    //Завдання 2
+    //Task 2
     int monthNumber;
-    cout << "Введіть номер місяця (1-12): " << endl;
+    cout << "Enter the month number (1-12): " << endl;
     cin >> monthNumber;
 
     if (monthNumber < 1 || monthNumber > 12) {
-        cout << "Невірний номер місяця!" << endl;
+        cout << "Number mount!" << endl;
         return 0;
     }
 
     switch (monthNumber) {
     case 1:
-        cout << "Січень має 31 день." << endl;
+        cout << "January has 31 days." << endl;
         break;
     case 2:
-        cout << "Лютий має 28 або 29 днів, залежно від року." << endl;
+        cout << "February has 28 or 29 days, depending on the year." << endl;
         break;
     case 3:
-        cout << "Березень має 31 день." << endl;
+        cout << "March has 31 days." << endl;
         break;
     case 4:
-        cout << "Квітень має 30 днів." << endl;
+        cout << "April has 30 days." << endl;
         break;
     case 5:
-        cout << "Травень має 31 день." << endl;
+        cout << "May has 31 days." << endl;
         break;
     case 6:
-        cout << "Червень має 30 днів." << endl;
+        cout << "June has 30 days." << endl;
         break;
     case 7:
-        cout << "Липень має 31 день." << endl;
+        cout << "July has 31 days." << endl;
         break;
     case 8:
-        cout << "Серпень має 31 день." << endl;
+        cout << "August has 31 days." << endl;
         break;
     case 9:
-        cout << "Вересень має 30 днів." << endl;
+        cout << "September has 30 days." << endl;
         break;
     case 10:
-        cout << "Жовтень має 31 день." << endl;
+        cout << "October has 31 days." << endl;
         break;
     case 11:
-        cout << "Листопад має 30 днів." << endl;
+        cout << "November has 30 days." << endl;
         break;
     case 12:
-        cout << "Грудень має 31 день." << endl;
+        cout << "December has 31 days." << endl;
         break;
     }
 
